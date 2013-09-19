75720ea1bf5dded286a1b5d3f1fdf238026e24ae
Tianyu Liu <poohlty@sina.com>
2013-09-19 12:06:05 -0400

    Install express, add .gitignore

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+var express = require('express');
 var http = require('http');
+var app = express();
 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello World\n");
+app.get('/', function(req, res){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello World from express\n");
 });
 
-server.listen(8000);
+app.listen(8000);
 
 console.log("Server running at http://localhost:8000/");
